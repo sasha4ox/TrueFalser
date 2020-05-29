57fc4384438dafe0b631150fc8230e5dd8a232df
sasha4ox <sasha4ox@gmail.com>
2020-05-30 01:11:10 +0300

    f

--- src/client-config.js
+++ src/client-config.js
@@ -1,6 +1,6 @@
 const apiUrl =
-    process.env.NODE_ENV === 'production'
-        ? 'https://true-falser-server.herokuapp.com/api'
-        : 'http://localhost:8080/api';
+  process.env.NODE_ENV === "production"
+    ? "https://true-falser-server.herokuapp.com/api"
+    : "http://localhost:8080/api";
 
 export { apiUrl };
